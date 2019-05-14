e5900e50484f053c89c903e552ffa1284a423e61
zygorun <zygorun@gmail.com>
2019-05-14 16:18:23 +0800

    update

--- src/main/java/com/gorun/repository/EmployeeRepository.java
+++ src/main/java/com/gorun/repository/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.gorun.repository;
 
 import com.gorun.domain.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
 
 import java.util.List;
@@ -12,5 +13,18 @@ public interface EmployeeRepository {//extends Repository<Employee,Integer> {
      Employee findByUsername(String username);
 
      //where username like ?% and age < ?
-     public List<Employee> findByUsernameStartingWithAndAgeLessThan(String username,Integer age);
+     List<Employee> findByUsernameStartingWithAndAgeLessThan(String username,Integer age);
+
+     //where username like %? and age < ?
+     List<Employee> findByUsernameEndingWithAndAgeLessThan(String username,Integer age);
+
+     //where username in(?,?...) or age < ?
+     List<Employee> findByUsernameInOrAgeLessThan(List<String> usernames,Integer age);
+
+     //where username in(?,?...) and age < ?
+     List<Employee> findByUsernameInAndAgeLessThan(List<String> usernames,Integer age);
+
+     //切记Employee是类名 不是employee表名
+     @Query("select o from Employee o where id = (select max(id) from Employee)")
+     Employee getEmployeeByMaxId();
 }
