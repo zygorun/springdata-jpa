16f497221d56016102b85826abffaa5a6212ce95
zygorun <zygorun@gmail.com>
2019-05-14 17:30:06 +0800

    update

--- src/main/java/com/gorun/repository/EmployeeRepository.java
+++ src/main/java/com/gorun/repository/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package com.gorun.repository;
 
 import com.gorun.domain.Employee;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.RepositoryDefinition;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -27,4 +29,23 @@ public interface EmployeeRepository {//extends Repository<Employee,Integer> {
      //切记Employee是类名 不是employee表名
      @Query("select o from Employee o where id = (select max(id) from Employee)")
      Employee getEmployeeByMaxId();
+
+     @Query("select o from Employee o where o.username = ?1 and o.age = ?2")
+     List<Employee> queryParams1(String username, Integer age);
+
+     @Query("select o from Employee o where o.username =:name and o.age =:age")
+     List<Employee> queryParams2(@Param("name") String username,@Param("age") Integer age);
+
+     @Query("select o from Employee o where o.username like %?1%")
+     List<Employee> queryLike1(String username);
+
+     @Query("select o from Employee o where o.username like %:name%")
+     List<Employee> queryLike2(@Param("name")String username);
+
+     @Query(nativeQuery = true,value = "select count(1) from employee")
+     long getCount();
+
+     @Modifying
+     @Query("update Employee o set username=:name where id=:id")
+     void update(@Param("name") String username,@Param("id") Integer id);
 }
